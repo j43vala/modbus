7239d67c0fd62ee8498270705a3f2020853608db
rutikdev0906 <rutikparmar445@gmail.com>
2023-11-04 09:25:28 +0530

    database connect error handle

--- test_postgres.py
+++ test_postgres.py
@@ -123,6 +123,7 @@
 #     # Handle the situation where the 'config' variable is not defined (e.g., provide default values or exit).
 #     pass
 
+
 import time
 import json
 import os
@@ -139,8 +140,12 @@ from datetime import datetime, timedelta
 # Initialize 'config' variable to None
 config = None
 
-# Read configuration data from a JSON file
-config_file_path = '/home/wzero/Public/modbus/w_script.json'
+# Specify the path to the configuration JSON file
+script_path = os.path.abspath(__file__)
+dir_path = os.path.dirname(script_path)
+
+config_file_path = os.path.join(dir_path, "config.json")
+sqlite_db_path = os.path.join(dir_path, "local.db")
 
 # Check if the configuration file exists
 if os.path.isfile(config_file_path):
@@ -150,8 +155,7 @@ else:
     print(f"Error: '{config_file_path}' not found")
     # Handle the case where the configuration file is missing (e.g., provide default values or exit).
 
-# Define paths and create a SQLite database and session
-sqlite_db_path = "/home/wzero/Public/modbus/mydatabase.db"
+# Define the SQLite database file path
 sqlite_engine = create_engine(f"sqlite:///{sqlite_db_path}", echo=True)
 print("SQLite database connected successfully")
 
@@ -173,12 +177,8 @@ non_written_data_delete_frequency_days = 1
 if config is not None:
     com_port = config["modbus"]["port"]
     devices = config["devices"]
-
-    # Define a dictionary to store the last_sync_index for each device
-    device_last_sync_index = {}
-    device_models = {}  # Dictionary to store dynamic models
-
-    # Create a PostgreSQL session to manage the 'last_sync_index' table
+    
+     # Create a PostgreSQL session to manage the 'last_sync_index' table
     LastSyncIndexBase = declarative_base()
 
     class LastSyncIndex(LastSyncIndexBase):
@@ -193,12 +193,13 @@ if config is not None:
     LastSyncIndexBase.metadata.create_all(sqlite_engine)
 
     # Create models and tables for each device during initialization
+    device_models = {}  # Dictionary to store dynamic models
+
     for device in devices:
-        hostname = socket.gethostname()
-        device_name = device.get("edge_device_name", "")
+        device_name = device.get("device_name", "")
         slave_id = device.get("slave_id", "")
-        table_name = f"{hostname}_{slave_id}_{device_name}"
-        register_list = device.get("registers")
+        table_name = f"{socket.gethostname()}_{slave_id}_{device_name}"
+        register_list = device.get("registers", "")
 
         # Create a dynamic database model for the device data
         model = create_dynamic_model(table_name, register_list)
@@ -207,17 +208,16 @@ if config is not None:
         device_models[device_name] = model
 
     # Function to synchronize data for a single device
-    def synchronize_data(device):
+    def synchronize_data(device_name):
         # Create sessions within the thread
         sqlite_session = get_sqlite_session()
         PostgresSQL_session = get_postgres_session()
 
-        # Get device information
-        hostname = socket.gethostname()
-        device_name = device.get("edge_device_name", "")
-        model = device_models.get(device_name)
-        
-        if model is not None:
+        device = next((device for device in devices if device.get("device_name", "") == device_name), None)
+        if device is not None:
+            hostname = socket.gethostname()
+            model = device_models[device_name]
+
             try:
                 while True:
                     # Query data from SQLite database for synchronization
@@ -236,7 +236,8 @@ if config is not None:
                             try:
                                 # Create a corresponding record in the PostgreSQL database
                                 postgres_data = model()
-                                for col_name in column_names:
+                                for register_info in device["registers"]:
+                                    col_name = register_info["column_name"]
                                     setattr(postgres_data, col_name, getattr(row, col_name))
                                 postgres_data.timestamp = row.timestamp
                                 PostgresSQL_session.add(postgres_data)
@@ -256,7 +257,7 @@ if config is not None:
 
                         # Commit the SQLite session
                         sqlite_session.commit()
-                        
+
                         # Delete data older than data_retention_period_minutes
                         current_time = datetime.utcnow()
                         data_retention_period = current_time - timedelta(minutes=data_retention_period_minutes)
@@ -271,12 +272,11 @@ if config is not None:
                             last_sync_index_record.last_deleted_at = current_time  # Update the last_deleted_at timestamp
                             sqlite_session.commit()
 
-
                     time.sleep(interval)
             except KeyboardInterrupt:
                 current_time = datetime.utcnow()
                 data_retention_period = current_time - timedelta(days=non_written_data_retention_period_days)
-                data_delete_frequency = current_time - timedelta(days=non_written_data_delete_frequency_minutes)
+                data_delete_frequency = current_time - timedelta(days=non_written_data_delete_frequency_days)
 
                 if data_delete_frequency > last_sync_index_record.last_deleted_at:
                     all_delete_data = sqlite_session.query(model).filter(model.timestamp < data_retention_period).all()
@@ -286,6 +286,7 @@ if config is not None:
 
                     last_sync_index_record.last_deleted_at = current_time  # Update the last_deleted_at timestamp
                     sqlite_session.commit()
+
                 # Close database sessions on keyboard interrupt
                 sqlite_session.close()
                 PostgresSQL_session.close()
@@ -294,7 +295,8 @@ if config is not None:
 
     # Create a separate thread for each device
     for device in devices:
-        thread = threading.Thread(target=synchronize_data, args=(device,))
+        device_name = device.get("device_name", "")
+        thread = threading.Thread(target=synchronize_data, args=(device_name,))
         thread.daemon = True
         thread.start()
 
